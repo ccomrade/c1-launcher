72cb61cdf3a16607ee879fd6c73e8d7ca4b15fbc
ccomrade <d.hryzbil@gmail.com>
2020-04-30 19:50:17 +0200

    Add ISystem and ILog

---
+++ Code/CryCommon/ILog.h
@@ -0,0 +1,57 @@
+// Copyright (C) 2001-2008 Crytek GmbH
+
+#pragma once
+
+#include <cstdarg>
+
+struct ILogCallback
+{
+	virtual void OnWriteToConsole(const char *text, bool newLine) = 0;
+	virtual void OnWriteToFile(const char *text, bool newLine) = 0;
+};
+
+struct ILog
+{
+	enum ELogType
+	{
+		eMessage,
+		eWarning,
+		eError,
+		eAlways,
+		eWarningAlways,
+		eErrorAlways,
+		eInput,
+		eInputResponse,
+		eComment
+	};
+
+	virtual void LogV(ELogType type, const char *format, va_list args) = 0;
+
+	virtual ~ILog() {}
+
+	virtual void Release() = 0;
+
+	virtual bool SetFileName(const char *filename) = 0;
+	virtual const char *GetFileName() = 0;
+
+	virtual void Log(const char *format, ...) = 0;
+	virtual void LogWarning(const char *format, ...) = 0;
+	virtual void LogError(const char *format, ...) = 0;
+
+	virtual void LogPlus(const char *format, ...) = 0;
+	virtual void LogToFile(const char *format, ...) = 0;
+	virtual void LogToFilePlus(const char *format, ...) = 0;
+	virtual void LogToConsole(const char *format, ...) = 0;
+	virtual void LogToConsolePlus(const char *format, ...) = 0;
+
+	virtual void UpdateLoadingScreen(const char *format, ...) = 0;
+
+	virtual void RegisterConsoleVariables() = 0;
+	virtual void UnregisterConsoleVariables() = 0;
+
+	virtual void SetVerbosity(int verbosity) = 0;
+	virtual int GetVerbosityLevel() = 0;
+
+	virtual void AddCallback(ILogCallback *pCallback) = 0;
+	virtual void RemoveCallback(ILogCallback *pCallback) = 0;
+};
--- Code/CryCommon/ISystem.h
+++ Code/CryCommon/ISystem.h
@@ -2,11 +2,48 @@
 
 #pragma once
 
-struct ISystem;
-struct ISystemUserCallback;
+#include <cstddef>
+
+struct I3DEngine;
+struct IAISystem;
+struct IAnimationGraphSystem;
+struct IBudgetingSystem;
+struct ICharacterManager;
+struct ICmdLine;
+struct IConsole;
+struct ICryFont;
+struct ICryPak;
+struct IDialogSystem;
+struct IEntitySystem;
+struct IFlowSystem;
+struct IFrameProfileSystem;
+struct IGame;
+struct IGlobalTaskScheduler;
+struct IGPUPhysicsManager;
+struct IHardwareMouse;
+struct IInput;
 struct ILog;
 struct ILogCallback;
+struct IMaterialEffects;
+struct IMemoryManager;
+struct IMovieSystem;
+struct IMusicSystem;
+struct INameTable;
+struct INetwork;
+struct IPhysicalWorld;
+struct IRenderer;
+struct IScriptSystem;
+struct ISoundSystem;
+struct IStreamEngine;
+struct ISystem;
+struct ISystemEventDispatcher;
+struct ISystemUserCallback;
+struct IThreadTaskManager;
+struct ITimer;
 struct IValidator;
+struct SSystemGlobalEnvironment;
+
+using std::size_t;
 
 /**
  * @brief Reverse engineered CryEngine initialization parameters.
@@ -31,4 +68,112 @@ struct SSystemInitParams
 	ISystem *pSystem;                    // initialized by IGameStartup::Init
 	void *pCheckFunc;                    // not used
 	void *pProtectedFunctions[10];       // probably not used
+};
+
+/**
+ * @brief The main engine interface.
+ * Initializes and dispatches all engine subsystems.
+ */
+struct ISystem
+{
+	struct ILoadingProgressListener
+	{
+		virtual void OnLoadingProgress(int steps) = 0;
+	};
+
+	virtual void Release() = 0;
+
+	virtual SSystemGlobalEnvironment *GetGlobalEnvironment() = 0;
+	virtual const char *GetRootFolder() const = 0;
+
+	virtual bool Update(int updateFlags = 0, int pauseMode = 0) = 0;
+
+	virtual void RenderBegin() = 0;
+	virtual void Render() = 0;
+	virtual void RenderEnd() = 0;
+	virtual void RenderStatistics() = 0;
+
+	virtual void *AllocMem(void *oldPtr, size_t newSize) = 0;
+	virtual unsigned int GetUsedMemory() = 0;
+
+	virtual const char *GetUserName() = 0;
+
+	virtual int GetCPUFlags() = 0;
+	virtual double GetSecondsPerCycle() = 0;
+
+	virtual void DumpMemoryUsageStatistics(bool useKB = false) = 0;
+
+	virtual void Quit() = 0;
+	virtual void Relaunch(bool relaunch) = 0;
+	virtual bool IsRelaunch() const = 0;
+	virtual void SerializingFile(int mode) = 0;
+	virtual int IsSerializingFile() const = 0;
+	virtual bool IsQuitting() = 0;
+
+	virtual void Error(const char *format, ...) = 0;
+	virtual void Warning(int module, int severity, int flags, const char *file, const char *format, ...) = 0;
+
+	virtual bool CheckLogVerbosity(int verbosity) = 0;
+
+	virtual bool IsDedicated() = 0;
+	virtual bool IsEditor() = 0;
+	virtual bool IsEditorMode() = 0;
+
+	virtual IConsole *GetIConsole() = 0;
+	virtual IScriptSystem *GetIScriptSystem() = 0;
+	virtual I3DEngine *GetI3DEngine() = 0;
+	virtual ISoundSystem *GetISoundSystem() = 0;
+	virtual IMusicSystem *GetIMusicSystem() = 0;
+	virtual IPhysicalWorld *GetIPhysicalWorld() = 0;
+	virtual IMovieSystem *GetIMovieSystem() = 0;
+	virtual IAISystem *GetAISystem() = 0;
+	virtual IMemoryManager *GetIMemoryManager() = 0;
+	virtual IEntitySystem *GetIEntitySystem() = 0;
+	virtual ICryFont *GetICryFont()	= 0;
+	virtual ICryPak *GetIPak() = 0;
+	virtual ILog *GetILog() = 0;
+	virtual ICmdLine *GetICmdLine() = 0;
+	virtual IStreamEngine *GetStreamEngine() = 0;
+	virtual ICharacterManager *GetIAnimationSystem() = 0;
+	virtual IValidator *GetIValidator() = 0;
+	virtual IFrameProfileSystem *GetIProfileSystem() = 0;
+	virtual INameTable *GetINameTable() = 0;
+	virtual IBudgetingSystem *GetIBudgetingSystem() = 0;
+	virtual IFlowSystem *GetIFlowSystem() = 0;
+	virtual IAnimationGraphSystem *GetIAnimationGraphSystem() = 0;
+	virtual IDialogSystem *GetIDialogSystem() = 0;
+	virtual IHardwareMouse *GetIHardwareMouse() = 0;
+	virtual ISystemEventDispatcher *GetISystemEventDispatcher() = 0;
+	virtual IGlobalTaskScheduler *GetIGlobalTaskScheduler() = 0;
+	virtual IThreadTaskManager *GetIThreadTaskManager() = 0;
+	virtual IGame *GetIGame() = 0;
+	virtual INetwork *GetINetwork() = 0;
+	virtual IRenderer *GetIRenderer() = 0;
+	virtual IInput *GetIInput() = 0;
+	virtual ITimer *GetITimer() = 0;
+
+	virtual IGPUPhysicsManager *GetIGPUPhysicsManager() = 0;
+
+	virtual void SetLoadingProgressListener(ILoadingProgressListener *pListener) = 0;
+	virtual ILoadingProgressListener *GetLoadingProgressListener() const = 0;
+
+	virtual void SetIGame(IGame *pGame) = 0;
+	virtual void SetIFlowSystem(IFlowSystem *pFlowSystem) = 0;
+	virtual void SetIAnimationGraphSystem(IAnimationGraphSystem *pAnimationGraphSystem) = 0;
+	virtual void SetIDialogSystem(IDialogSystem *pDialogSystem) = 0;
+	virtual void SetIMaterialEffects(IMaterialEffects *pMaterialEffects) = 0;
+
+	virtual void ChangeUserPath(const char *userPath) = 0;
+
+	virtual void DebugStats(bool checkpoint, bool leaks) = 0;
+	virtual void DumpWinHeaps() = 0;
+	virtual int DumpMMStats(bool log) = 0;
+
+	virtual void SetForceNonDevMode(bool bValue) = 0;
+	virtual bool GetForceNonDevMode() const = 0;
+	virtual bool WasInDevMode() const = 0;
+	virtual bool IsDevMode() const = 0;
+	virtual bool IsMODValid(const char *modName) const = 0;
+
+	// ...
 };
